7d3aa108ff33aada89dee9344367ad98c671da3b
Jrad messaoud <jradmassoud@gmail.com>
2025-07-07 16:53:39 +0100

    Delete run_app.ps1

--- run_app.ps1
+++
@@ -1,4 +0,0 @@
-Write-Host "🫒 Lancement de Olive Oil Tracker..." -ForegroundColor Green
-Write-Host ""
-python -m streamlit run app.py --server.port 8501
-Read-Host "Appuyez sur Entrée pour fermer" 
